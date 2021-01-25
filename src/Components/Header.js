91f01885bf610b4a18d9e907089b666362c20270
daniel-takacs <takacsd89@gmail.com>
2021-01-25 19:41:46 +0200

    overflow hidden and 10px padding added to app, before line added to footer

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -15,24 +15,20 @@ function Header() {
         <div className="header">
             <Navbar />
                 <div className="landingpage">
-                <div className="header-pattern-right"><img src={HeaderPatternRight} alt=""/></div>
+                    <div className="header-pattern-right"><img src={HeaderPatternRight} alt=""/></div>
                     <div className="image-family"><img src={ImageFamily} alt=""/></div>
-
-
                     <div className="landingtextcontainer">
-            <div className="landingtext">
-            <div className="pattern1"><img src={PatternIntroLeftMobile} alt=""/></div>
-            <h1>Humanizing your insurance.</h1>  
-            <p> Get your life insurance coverage easier and faster. 
-                We blend our expertise and technology to help you find 
-                the plan that’s right for you. Ensure you and your loved ones are protected. 
-            </p> 
-            <div className="landing-button"><button>VIEW PLANS</button></div> 
-            <div className="pattern2"><img src={PatternIntroRightMobile} alt=""/></div>
-            </div>
-        </div>
-
-
+                        <div className="landingtext">
+                            <div className="pattern1"><img src={PatternIntroLeftMobile} alt=""/></div>
+                            <h1>Humanizing your insurance.</h1>  
+                            <p> Get your life insurance coverage easier and faster. 
+                                We blend our expertise and technology to help you find 
+                                the plan that’s right for you. Ensure you and your loved ones are protected. 
+                            </p> 
+                            <div className="landing-button"><button>VIEW PLANS</button></div> 
+                            <div className="pattern2"><img src={PatternIntroRightMobile} alt=""/></div>
+                        </div>
+                    </div>
                     <div className="image-family-desktop"><img className="dimage"src={ImageFamilyDesktop} alt=""/></div>
                     <div className="header-pattern-left"><img src={HeaderPatternLeft} alt=""/></div>
 
