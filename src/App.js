6d73563e5d156b74725619cbd188740185a0078b
daniel-takacs <takacsd89@gmail.com>
2021-01-25 17:01:27 +0200

    header h1 before line added, right pattern added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import './App.css';
 import Header from './Components/Header';
 import Main from './Components/Main';
 import Footer from './Components/Footer';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="app">
+      <Router>
         <Header />
         <Main />
         <Footer />
+                <Switch>
+                    <Route path='/' />
+                </Switch>
+            </Router>
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,19 +4,15 @@ import './Header.css'
 import ImageFamily from './images/image-intro-mobile.jpg';
 import ImageFamilyDesktop from './images/image-intro-desktop.jpg';
 import HeaderText from './HeaderText';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import HeaderPatternRight from './images/bg-pattern-intro-right-desktop.svg'
 
 
 function Header() {
     return (
         <div className="header">
-            <Router>
             <Navbar />
-                <Switch>
-                    <Route path='/' />
-                </Switch>
-            </Router>
                 <div className="landingpage">
+                <div className="header-pattern-right"><img src={HeaderPatternRight} alt=""/></div>
                     <div className="image-family"><img src={ImageFamily} alt=""/></div>
                     <HeaderText />
                     <div className="image-family-desktop"><img className="dimage"src={ImageFamilyDesktop} alt=""/></div>
