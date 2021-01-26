ca0cdf2d66f42b3019b605094ec83e713be71532
daniel-takacs <takacsd89@gmail.com>
2021-01-26 11:36:40 +0200

    nav-list styled, nav-list button border white, hamburger active class added, close icon moved to navbar, navmenu pattern added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,6 +3,7 @@ import closeIcon from './images/icon-close.svg';
 import hamburgerIcon from './images/icon-hamburger.svg';
 import './Navbar.css';
 import Logo from './images/logo.svg';
+import NavPattern from './images/bg-pattern-mobile-nav.svg';
 
 
 function Navbar() {
@@ -12,12 +13,16 @@ function Navbar() {
     
     return (
         <div className="navbar">
-            <img src={Logo} alt=""/>
-            <img className="hamburger-icon"src={hamburgerIcon} alt="hamburgericon" onClick={showSidebar}/>
-                <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+            <div className="logo">
+                <img src={Logo} alt=""/>
+            </div>
+            <button className={sidebar ? "hamburger active" : "hamburger"}>
+                <img className="hamburger-icon"src={hamburgerIcon} alt="hamburgericon" onClick={showSidebar}/>
+                <img className="close-icon" src={closeIcon} alt="closeicon" onClick={showSidebar}/>
+            </button>                
+            <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
                     <ul className="nav-list">
                         <li>
-                            <img className="close-icon" src={closeIcon} alt="closeicon" onClick={showSidebar}/>
                         </li>
                         <li>
                             <a href="">How we work</a>
@@ -29,9 +34,11 @@ function Navbar() {
                             <a href="">Account</a>
                         </li>
                         <li>
-                           <a className="nav-button" href="">View Plans</a>
+                           <a className="nav-button nav-menu-button" href="">View Plans</a>
                         </li>
+                        <img className="nav-pattern" src={NavPattern} alt="navpattern"/>
                     </ul>
+                    
             </nav>
         </div>
     )
